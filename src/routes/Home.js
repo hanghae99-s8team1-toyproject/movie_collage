e5d2e61e2da4335dbf137bd84d23cb22cba9f772
Minji Jeon <minji799@gmail.com>
2022-06-30 23:25:22 +0900

    test commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -31,7 +31,6 @@ function Home(){
 
   return(
     <div className="container">
-      <p></p>
     {loading ? <div className="Loading">Loading...</div> : 
       <>
         <div className="container">
