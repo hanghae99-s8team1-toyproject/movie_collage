6218d7688767c3fe2979b718aab680454ddd5582
Minji Jeon <minji799@gmail.com>
2022-07-08 17:19:44 +0900

    로딩화면추가

--- src/routes/Test.js
+++ src/routes/Test.js
@@ -15,8 +15,10 @@ function Test() {
           // json 내 movies 오브젝트 속 내용들을 Movies에 저장하기로 했습니다.
           setMovies(json.movies);
         });
+
       // movies의 내용이 있을 때!
     } else {
+      스위치변경(false);
       // console.log(movies);
     }
   }, [movies]);
@@ -26,7 +28,7 @@ function Test() {
   const [질문] = useState(Question);
   const [번호, 번호변경] = useState(0);
   const [진행바, 진행바변경] = useState((100 * (번호 + 1)) / 질문.length);
-
+  const [스위치, 스위치변경] = useState(true);
   const onClick = (event) => {
     let keyword = event.target.value;
     let 결과 = movies.filter((영화) => {
@@ -55,35 +57,53 @@ function Test() {
   return (
     <div className="App">
       <header className="header">모두의 영화</header>
-      <section className={"content start "}>
-        <div className="content--question">
-          <p className="content--question__box">{질문[번호].q}</p>
-
-          <ProgressBar
-            앞으로={앞으로}
-            번호={번호}
-            onClick={onClick}
-            진행바={진행바}
-          />
-        </div>
-        <div className="content--answer">
-          <button
-            className="content--answer__btn"
-            value={필터[번호].b1}
-            onClick={onClick}
-          >
-            {질문[번호].b1}
-          </button>
-          <button
-            className="content--answer__btn"
-            value={필터[번호].b2}
-            onClick={onClick}
-          >
-            {질문[번호].b2}
-          </button>
-        </div>
-      </section>
+      {스위치 ? (
+        <Loading />
+      ) : (
+        <TestSection
+          필터={필터}
+          질문={질문}
+          번호={번호}
+          진행바={진행바}
+          스위치={스위치}
+          onClick={onClick}
+          앞으로={앞으로}
+        />
+      )}
     </div>
+  );
+}
+
+function TestSection({ 필터, 질문, 번호, 진행바, onClick, 앞으로 }) {
+  return (
+    <section className={"content start "}>
+      <div className="content--question">
+        <p className="content--question__box">{질문[번호].q}</p>
+
+        <ProgressBar
+          앞으로={앞으로}
+          번호={번호}
+          onClick={onClick}
+          진행바={진행바}
+        />
+      </div>
+      <div className="content--answer">
+        <button
+          className="content--answer__btn"
+          value={필터[번호].b1}
+          onClick={onClick}
+        >
+          {질문[번호].b1}
+        </button>
+        <button
+          className="content--answer__btn"
+          value={필터[번호].b2}
+          onClick={onClick}
+        >
+          {질문[번호].b2}
+        </button>
+      </div>
+    </section>
   );
 }
 
@@ -99,6 +119,28 @@ function ProgressBar(props) {
       </span>
       <div className="프로그래스바--container">
         <progress value={props.진행바} max="100"></progress>
+      </div>
+    </div>
+  );
+}
+
+function Loading() {
+  return (
+    <div>
+      <p>로딩중입니다...</p>
+      <div class="sk-fading-circle">
+        <div class="sk-circle1 sk-circle"></div>
+        <div class="sk-circle2 sk-circle"></div>
+        <div class="sk-circle3 sk-circle"></div>
+        <div class="sk-circle4 sk-circle"></div>
+        <div class="sk-circle5 sk-circle"></div>
+        <div class="sk-circle6 sk-circle"></div>
+        <div class="sk-circle7 sk-circle"></div>
+        <div class="sk-circle8 sk-circle"></div>
+        <div class="sk-circle9 sk-circle"></div>
+        <div class="sk-circle10 sk-circle"></div>
+        <div class="sk-circle11 sk-circle"></div>
+        <div class="sk-circle12 sk-circle"></div>
       </div>
     </div>
   );
